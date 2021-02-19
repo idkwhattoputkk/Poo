56e40e8ac59cb7a319980355a50756f5e32e8035
Emanuel Umaña <emanuelumana@MacBook-Pro-de-Emanuel.local>
2021-02-18 22:08:07 -0500

    finishing the third exercise

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
+struct contact
+{
+  int number;
+  string name;
+};
+
 int main(){
-    return 0;
+
+	struct contact conts[5];
+  	int i;
+
+	for(i=0; i<5; i++){                   		//taking values from user
+		cout << "contact " << i + 1 << endl;
+		cout << "Enter his number" << endl;
+		cin >> conts[i].number;
+		cout << "Enter name" << endl;
+		cin >> conts[i].name;
+	}
+
+	for(i=0; i<5; i++){							//printing values
+		cout << "Contact " << i + 1 << endl;
+		cout << "number : " << conts[i].number << endl;
+		cout << "Name : " << conts[i].name << endl;
+	return 0;
 }
