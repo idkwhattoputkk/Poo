c082c064b0ed587af9b00e94e509d1e31e7df418
Emanuel Umaña <emanuelumana@MacBook-Pro-de-Emanuel.local>
2021-04-08 23:48:07 -0500

    done with the Critrerio's basics

--- Proyecto_POO_1/model/Criterio.cpp
+++ Proyecto_POO_1/model/Criterio.cpp
@@ -9,3 +9,21 @@ Criterio::Criterio(string observacion, double porcentaje, int id){
     this->id = id; 
     
 }
+string Criterio::getObservacion(){
+    return observacion;
+}
+void Criterio::setObservacion(string observacion){
+    this->observacion = observacion;
+}
+double Criterio::getPorcentaje(){
+    return porcentaje;
+}
+void Criterio::setPorcentaje(double portcentaje){
+    this->porcentaje = porcentaje;
+}
+int Criterio::getId(){
+    return id;
+}
+void Criterio::setId(int id){
+    this->id = id;
+}
