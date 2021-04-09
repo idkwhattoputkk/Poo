2ec4e0b356b5a3680aa366e26fe81b401af2c9d2
Emanuel Umaña <emanuelumana@MacBook-Pro-de-Emanuel.local>
2021-04-08 20:19:22 -0500

    Coding the proyect

---
+++ Proyecto_POO_1/model/Acta.h
@@ -0,0 +1,37 @@
+#ifndef ACTA_H
+#define ACTA_H
+#include<iostream>
+#include<string>
+using namespace std;
+class Acta{
+private:
+	int numero;
+	string fecha;
+	string autor;
+	string nombre;
+    enum tipo;
+	int periodo;
+	string director;
+	string codirector;
+	enum estado;
+	double notaFinal;
+public:
+    Acta();
+    int getNumero();
+    void setNumero(int numero);
+    string getFecha();
+    void setFecha(string fecha);
+    string getAutor();
+    void setAutor(string autor);
+    string getNombre();
+    void setNombre(string nombre);
+    int getPeriodo();
+    void setPeriodo(int periodo);
+    string getDirector();
+    void setDirector(string director);
+    string getCodirector();
+    void setCodirector(string codirector);
+    double getnotaFinal();
+    void setnotaFinal(double notaFinal);
+};
+#endif
