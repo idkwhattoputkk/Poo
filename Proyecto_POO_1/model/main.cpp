d454fa5934880f6add203b85a048877f9667f685
Emanuel Umaña <emanuelumana@MacBook-Pro-de-Emanuel.local>
2021-04-09 10:48:13 -0500

    adding the menu for the code

--- Proyecto_POO_1/model/main.cpp
+++ Proyecto_POO_1/model/main.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <string>
 #include "Acta.h"
+using namespace std;
+
+int menu()
+{
+    int opcion;
+    cout << "Menu Actas\n"<< endl;
+    cout << "1. Agregar Actas" << endl;
+    cout << "0. Salir\n" <<endl;
+    cout << "Digita el numero: " << endl;
+    cin >> opcion;
+    return opcion;
+}
